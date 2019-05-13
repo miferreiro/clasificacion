f265a2a0ec575dfb6406622a614319ee2bede913
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-05-13 20:25:25 +0200

    Adds new plots

---
+++ analysis/analysis-CHI2-IG.R
@@ -0,0 +1,14 @@
+file = "results/eml-ig.rds"
+chi2 <- readRDS(file = file)
+chi2 <- arrange(chi2,-attr_importance)
+plot(type = "l", density(chi2$attr_importance), main = "Eml IG", xlab = "importance", ylab = "Density")
+cut.off.chi2 = 2000
+abline(v = chi2$attr_importance[cut.off.chi2], col = "blue", lty = 2)
+
+density <- density(chi2$attr_importance)
+min <- min(which(density$x >= chi2$attr_importance[cut.off.chi2]))
+max <- max(which(density$x <= chi2$attr_importance[1]))
+with(density, polygon(x = c(x[c(min, min:max, max)]), y = c(0,y[min:max], 0), col = rgb(0, 206/255, 209/255, 0.5)))
+
+
+
